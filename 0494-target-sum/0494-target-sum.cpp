095fc008a51cc6ffad8f1b4b755fbccbbcd25af7
Yash Chauhan <yr8365865@gmail.com>
2026-07-30 23:33:54 +0530

    Time: 18 ms (44.78%), Space: 25.7 MB (26.64%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int sum;
-    int Ways(int i,vector<int>& arr,int res,int target){
+    int Ways(int i,vector<int>& arr,int res,int target,vector<vector<int>>& dp){
         if(i==arr.size()){
             if(res==target) return 1;
             else return 0;
         }
-        int add=Ways(i+1,arr,res+arr[i],target);
-        int sub=Ways(i+1,arr,res-arr[i],target);
+        if(dp[i][res+sum]!=-1) return dp[i][res+sum];
+        int add=Ways(i+1,arr,res+arr[i],target,dp);
+        int sub=Ways(i+1,arr,res-arr[i],target,dp);
 
-        return add+sub;
+        return dp[i][res+sum]=add+sub;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         sum=0;
         int n=nums.size();
         for(int ele:nums) sum+=ele;
-        return Ways(0,nums,0,target);
+        vector<vector<int>> dp(n,vector<int>(2*sum+1,-1));
+        return Ways(0,nums,0,target,dp);
     }
 };
