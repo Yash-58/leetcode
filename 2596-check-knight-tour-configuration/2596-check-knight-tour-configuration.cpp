cb2f298f7b4cea266d69bb016a40d2a94a13e080
Yash Chauhan <yr8365865@gmail.com>
2026-08-07 23:51:47 +0530

    Time: 0 ms (100%), Space: 31.6 MB (89.48%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool helper(vector<vector<int>>& grid,int row,int col, int num){
+        int n=grid.size();
+        if(grid[row][col]==n*n-1) return true;
+        int i,j;
+        i=row-2;
+        j=col+1;
+        if(i>=0 && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        i=row-2;
+        j=col-1;
+        if(i>=0 && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        i=row+2;
+        j=col+1;
+        if(i<n && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        i=row+2;
+        j=col-1;
+        if(i<n && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        i=row-1;
+        j=col-2;
+        if(i>=0 && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        i=row+1;
+        j=col-2;
+        if(i<n && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        i=row-1;
+        j=col+2;
+        if(i>=0 && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        i=row+1;
+        j=col+2;
+        if(i<n && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+
+        return false;
+    }
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        if(grid[0][0]!=0) return false;
+        return helper(grid,0,0,0);
+        
+    }
+};
